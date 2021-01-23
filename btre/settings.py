c849011e1daa175e4454daf0ac1cdaabe478c46a
Rajat_dudeja1 <rajat_dudeja@dell.com>
2021-01-23 20:36:54 +0530

    Removed email from settings

--- btre/settings.py
+++ btre/settings.py
@@ -142,14 +142,6 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
 
-# Email config (NOT COMPLETE)
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'inquiry.primesolutions@gmail.com'
-EMAIL_HOST_PASSWORD = 'testing@123'
-EMAIL_USE_TLS = True
-
 
 try:
     from .local_settings import *
