b194dd17f58f6d200fa92b671b6a1adf6e6e728f
Rajat_dudeja1 <rajat_dudeja@dell.com>
2021-01-23 21:01:42 +0530

    isPublished to is_published in listing views

--- listings/views.py
+++ listings/views.py
@@ -5,7 +5,7 @@ from listings.choices import price_choices, bedroom_choices, state_choices
 
 
 def index(request):
-    listings = Listing.objects.order_by('-list_date').filter(isPublished=True)
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
     paginator = Paginator(listings, 6)
     page = request.GET.get('page')
     paged_listings = paginator.get_page(page)
